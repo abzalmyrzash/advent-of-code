df9e6382dac34663076d5eb6f05e8249bf77ac35
Abzal Myrzash <abzal.myrzash006@gmail.com>
2025-12-29 19:40:38 +0500

    12

---
+++ makefile
@@ -0,0 +1,2 @@
+part1: part1.c
+	gcc part1.c -o part1 -std=c2x
--- part1.c
+++ part1.c
@@ -7,6 +7,8 @@ constexpr int P_WIDTH = 3;
 constexpr int P_LENGTH = 3;
 
 char presents[MAX_PRESENTS][P_LENGTH][P_WIDTH];
+int p_areas[MAX_PRESENTS] = { 0 };
+
 int num_presents = 0;
 
 typedef struct {
@@ -23,10 +25,11 @@ int main() {
 	int p = 0;
 	int x;
 	int ret;
+	char ch;
 	while (1) {
 		ret = fscanf(file, "%d", &x);
 		assert(ret == 1);
-		char ch = getc(file);
+		ch = getc(file);
 		if (ch == 'x') break;
 		assert(x == p);
 		assert(ch == ':');
@@ -38,6 +41,7 @@ int main() {
 				ch = getc(file);
 				assert(ch == '#' || ch == '.');
 				presents[p][i][j] = ch;
+				if (ch == '#') p_areas[p]++;
 			}
 			ch = getc(file);
 			assert(ch == '\n');
@@ -47,7 +51,7 @@ int main() {
 		p++;
 	}
 	num_presents = p;
-
+/*
 	for (int p = 0; p < num_presents; p++) {
 		for (int i = 0; i < P_LENGTH; i++) {
 			for (int j = 0; j < P_WIDTH; j++) {
@@ -57,20 +61,44 @@ int main() {
 		}
 		printf("\n");
 	}
-
+*/
 	regions[0].length = x;
 	int r = 0;
+	int cnt = 0;
 	while (1) {
 		ret = fscanf(file, "%d", &x);
 		assert(ret == 1);
 		regions[r].width = x;
-		if (fscanf(file, "%d", &x) != 1) break;
+		int area = regions[r].length * regions[r].width;
+		ch = getc(file);
+		assert(ch == ':');
+		int sum = 0;
+		for (int i = 0; i < num_presents; i++) {
+			ret = fscanf(file, "%d", &x);
+			assert(ret == 1);
+			regions[r].presents[i] = x;
+			sum += x * p_areas[i];
+		}
+		if (sum <= area) cnt++;
 		r++;
+		if (fscanf(file, "%d", &x) != 1) break;
 		regions[r].length = x;
 		ch = getc(file);
 		assert(ch == 'x');
 	}
 	num_regions = r;
+	printf("%d\n", cnt);
+
+	/*
 	printf("%d\n", num_regions);
+
+	for (int r = 0; r < num_regions; r++) {
+		printf("%dx%d: ", regions[r].length, regions[r].width);
+		for (int p = 0; p < num_presents; p++) {
+			printf("%d ", regions[r].presents[p]);
+		}
+		printf("\n");
+	}
+	*/
 }
 
